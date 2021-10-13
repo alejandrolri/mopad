611c05b76c6b09bca0c1a3bb8df4452448d16bd7
alejandrolri <82502050+alejandrolri@users.noreply.github.com>
2021-10-13 13:13:12 +0200

    Incluidas nuevas funcionalidades

--- mopad.sh
+++ mopad.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
+x-terminal-emulator -geometry 60x15+50+50 -e roscore
+x-terminal-emulator -geometry 60x15+50+50 -e ssh mopad@10.42.0.1 roslaunch mopad_bringup all.launch
+sleep 2.0
 PS3='Seleccione : '
-scripts=("Mapeado" "Guardar mapa" "Localización" "Elegir ruta" "Navegación" "Salir")
+scripts=("Mapeo" "Guardar mapa" "Elegir ruta" "Localización"	 "Navegación(rviz)" "Navegación(ruta)" "PTU" "Salir")
 select fav in "${scripts[@]}"; do
     case $fav in
-        "Mapeado")
+        "Mapeo")
             echo "Iniciando proceso de mapeado..."
-	    ./map.sh		#Script de mapeado
+	    ./map.sh		#Script de mapeo
             ;;
 	"Guardar mapa")
-            echo "Abriendo mapa..."
-	    roslaunch mopad_navigation save_map.launch
+            echo "Guardando mapa..."
+	    x-terminal-emulator -geometry 10x10+900+50 -e roslaunch mopad_navigation save_map.launch			#Guardar mapa. Mapeo debe estar iniciado.	
 	    ;;
         "Localización")
             echo "Iniciando proceso de localización..."
-	    ./localization.sh	#Script de localización
+	    ./localization.sh	#Script de localización. Necesario para navegación.
             ;;
 	"Elegir ruta")
             echo "Abriendo mapa..."
-	    roslaunch mopad_navigation select_route.launch #Seleccionar ruta
+	    x-terminal-emulator -geometry 10x10+900+50 -e roslaunch mopad_navigation select_route.launch #Seleccionar ruta
 	    ;;
-        "Navegación")
+        "Navegación(rviz)")
             echo "Iniciando proceso de navegación..."
-	    ./navigation.sh	#Script de navegación
-	    #break
+	    ./navigation_rviz.sh	#Script de navegación mediante rviz sin PTU
             ;;
+	"Navegación(ruta)")
+            echo "Iniciando proceso de navegación..."
+	    ./navigation_route.sh	#Script de navegación mediante ruta+PTU
+            ;;
+	"PTU")
+	    echo "Iniciando proceso de PTU..."
+	    ./PTU.sh			#Script de movimiento de PTU
+	    ;;
 	"Salir")
 	    echo "Saliendo..."
 	    exit
---
+++ navigation_rviz.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#x-terminal-emulator -geometry 10x10+900+50 -e roslaunch mopad_bringup rtabmap.launch localization:=true
+#x-terminal-emulator -geometry 60x15+50+50 -e ssh mopad@10.42.0.1 roslaunch mopad_bringup all.launch
+#x-terminal-emulator -geometry 60x15+50+50 -e ssh mopad@10.42.0.1 roslaunch asr_flir_ptu_driver ptu_left.launch 
+x-terminal-emulator -geometry 60x15+50+410 -e roslaunch mopad_navigation move_base.launch	#Primero cerrar teleop para evitar conflictos
+sleep 1.0
+x-terminal-emulator -geometry 60x15+1000+410 -e roslaunch mopad_rviz navigation_rviz.launch 
+
+
+
