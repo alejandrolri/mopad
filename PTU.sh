fefbd6e40ed2cbc69267f9b9a15d1d1d78f2fff0
alejandrolri <82502050+alejandrolri@users.noreply.github.com>
2021-10-20 09:43:07 +0200

    Delete PTU.sh

--- PTU.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-x-terminal-emulator -geometry 60x15+50+50 -e ssh mopad@10.42.0.1 roslaunch asr_flir_ptu_driver ptu_left.launch 
-sleep 50.0
-x-terminal-emulator -geometry 10x10+900+50 -e roslaunch mopad_navigation ptu.launch
