f998edd9c20cb5085b782cfb00763d35353ad13a
alejandrolri <82502050+alejandrolri@users.noreply.github.com>
2022-03-23 09:51:25 +0100

    Delete localization.sh

--- localization.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-x-terminal-emulator -geometry 10x10+900+50 -e roslaunch mopad_bringup rtabmap.launch $1 localization:="true"  
-x-terminal-emulator -geometry 60x15+50+410 -e roslaunch mopad_teleop keyboard_teleop.launch
-sleep 2.0
-x-terminal-emulator -geometry 60x15+1000+410 -e roslaunch mopad_rviz mapping_rviz.launch 
